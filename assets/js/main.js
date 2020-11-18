63682e8db65ea56265e3a8c3450995aaf1b63e24
Finnschy <skylerfearless@yahoo.de>
2020-11-18 20:47:35 +0100

    funktioniert endlich

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,43 +7,6 @@ let roundsCounter = document.getElementById("roundsCounter")
 let rounds = document.getElementsByName("rounds")
 let startGuessing = document.getElementById("start")
 
-
-//--- Funktion um die Spielrunden zu bestimmen
-function checkRounds() {
-    let four = document.getElementById("four")
-    let five = document.getElementById("five")
-    let six = document.getElementById("six")
-    let custom = document.getElementById("custom")
-    let customInput = document.getElementById("customInput").value
-    let radioRounds = 0
-    if (four.checked) {
-        radioRounds = 4
-    } else if (five.checked) {
-        radioRounds = 5 
-    } else if (six.checked) {
-        radioRounds = 6
-    } else if (custom.checked) {
-        radioRounds = customInput
-        document.getElementById("customInput").classList.remove("notDisplayed")
-    }
-    return radioRounds
-}
-checkRounds()
-
-
-//--- Funktion um den Value aus den Radio Buttons zu kriegen -- funktioniert so nicht
-// function checkRounds() {
-//     for (var i = 0, length = rounds.length; i < length; i++) {
-//         rounds[i].onclick = function() {
-//             // return this.value
-//             console.log(this.value);
-
-//         }
-//       }
-// }
-// checkRounds()
-
-
 //--- Funktion für den Zufallsgenerator
 
 const randomizer = () => {
@@ -53,26 +16,127 @@ const randomizer = () => {
 let compNumber = randomizer()
 console.log(compNumber);
 
+//--- Funktion um den Value aus den Radio Buttons zu kriegen -- funktioniert jetzt aber ich muss alles nochmal klicken
+let valueCollec = [];
+function checkRounds() {
+    let custom = document.getElementById("custom");
+    let customInput = document.getElementById("customInput");
+    
+    for (let i = 0; i < rounds.length; i++) {
+        rounds[i].onclick = function() {
+            let radioRounds
+            // return this.value
+            // console.log(this.value);
+            // radioRounds = this.value
+            // console.log(radioRounds);
+            valueCollec = this.value;
+            console.log(valueCollec);
+                if(valueCollec == 0) {
+                    console.log("if schlaufe test");
+                    customInput.classList.toggle("notDisplayed");
+                    // radioRounds = customInput.value
+                    customInput.addEventListener("keyup", () => {
+
+                        valueCollec = customInput.value;
+                        // showHelp(item.value);
+                        console.log(valueCollec);
+                    })
+                    
+                }
+        }
+      }
+}
+checkRounds()
+
+
+//--- TEST -- ich hab alle ab "let clickcount" in die funktion "showhelp" kopiert, dh wenn da gleich wieder etwas nicht mehr funktioniert, muss ich das einfach wieder löschen und unten auskommentieren....
+// function showHelp(help) {
+//     let radioRounds = help;
+//     console.log(radioRounds);
+
+//     let clickCount = 0
+//     startGuessing.addEventListener("click", () => {
+//     event.preventDefault()
+//     clickCount++
+//     // let radioRounds = checkRounds()
+//     // let radioRounds = valueCollec
+//     roundsCounter.innerHTML = `${clickCount}/${radioRounds}` 
+//     let guessingInput = document.getElementById("guessingInput")
+
+//     if (clickCount == radioRounds) {
+//         result.innerHTML += `${clickCount}. You didn't guessed right. You lose! <br>`
+//         console.log("Test lose");
+//     } else if (guessingInput.value > compNumber) {
+//         result.innerHTML += `${clickCount}. You need to guess lower than ${guessingInput.value}. Try again. <br>`
+//         console.log("Test need to guess lower");
+//     } else if (guessingInput.value < compNumber) {
+//         result.innerHTML += `${clickCount}. You need to guess higher than ${guessingInput.value}. Try again. <br>`
+//         console.log("Test need to guess higher");
+//     } else if (guessingInput.value == compNumber) {
+//         result.innerHTML += `${clickCount}. You got me in ${clickCount} guesses. You win! <br>`
+//         console.log("Test win");
+//     }
+
+//     // countingRounds()
+// })
+    
+//   }
+
+//   function setupHelp() {
+//     for (let i = 0; i < rounds.length; i++) {
+//       let item = rounds[i];
+//       item.onclick = function() {
+//         showHelp(item.value);
+//         if(item.value == 0) {
+//               console.log("if schlaufe test");
+//              customInput.classList.toggle("notDisplayed");
+//             // radioRounds = customInput.value
+//             // mouseleave or keyup
+//             customInput.addEventListener("mouseleave", () => {
+//                 item.value = customInput.value;
+//                 showHelp(item.value);
+//             })
+             
+//           }
+//       }
+//     }
+//   }
+//   setupHelp();
+
+//-------------------------------------------------------------
+
 //--- Funktion für das eigentliche Spiel
 let clickCount = 0
 startGuessing.addEventListener("click", () => {
+
     event.preventDefault()
     clickCount++
-    let radioRounds = checkRounds() 
+    // let radioRounds = checkRounds()
+    let radioRounds = valueCollec
+
     roundsCounter.innerHTML = `${clickCount}/${radioRounds}` 
     let guessingInput = document.getElementById("guessingInput")
-
-
-    if (clickCount === radioRounds) {
+    if (clickCount == radioRounds) {
         result.innerHTML += `${clickCount}. You didn't guessed right. You lose! <br>`
+
+        // setTimeout(() => {
+        //     result.innerHTML = ""
+        //     roundsCounter.innerHTML = ""
+        // }, 3000);
+
     } else if (guessingInput.value > compNumber) {
         result.innerHTML += `${clickCount}. You need to guess lower than ${guessingInput.value}. Try again. <br>`
     } else if (guessingInput.value < compNumber) {
         result.innerHTML += `${clickCount}. You need to guess higher than ${guessingInput.value}. Try again. <br>`
     } else if (guessingInput.value == compNumber) {
         result.innerHTML += `${clickCount}. You got me in ${clickCount} guesses. You win! <br>`
+
+        // setTimeout(() => {
+        //     result.innerHTML = ""
+        //     roundsCounter.innerHTML = ""
+        // }, 3000);
     }
-    
+
     // countingRounds()
 })
 
